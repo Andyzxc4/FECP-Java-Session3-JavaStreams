e4d3fc456596cc3eb4f408a4e63ea9553bb2815b
Andre Dominic Lacra <andre-d.lacra@GXCHLAP00688.local>
2025-07-07 13:20:44 +0800

    feat: added codes for Activity 2 & clean up output printing

--- src/main/java/org/example/JavaStreams.java
+++ src/main/java/org/example/JavaStreams.java
@@ -10,17 +10,36 @@ public class JavaStreams {
     public static void main(String[] args) {
 
         // ~~~~~ Activity 1 ~~~~~
-        List <Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         Predicate<Integer> isOdd = n -> n % 2 != 0;
 
         //  defined allOdds List with Integer types that stores all ODD numbers in list1 List
-        List <Integer> allOdds = list1.stream()
+        List<Integer> allOdds = list1.stream()
                 .filter(isOdd)
                 .collect(Collectors.toList());
 
         //~~~~~ Output for Activity 1 ~~~~~
-        System.out.println(allOdds);
+        System.out.println("Activity 1");
+        System.out.printf("Initial list: %s\n", list1);
+        System.out.printf("List of all Odds: %s\n", allOdds);
+
+        // ============================
+
+        // ~~~~~ Activity 2 ~~~~~
+        List<String> names = Arrays.asList("Alice", "Adam", "Bob", "Angela", "Brian");
+
+        Predicate<String> startsWithA = x -> x.startsWith("A");
+
+        List<String> listStartsWithA = names.stream()
+                .filter(startsWithA)
+                .toList();
+
+        //~~~~~ Output for Activity 2 ~~~~~
+        System.out.println("\nActivity 2");
+        System.out.printf("Initial name list: %s\n", names);
+        System.out.printf("List of names start with 'A': %s\n", listStartsWithA);
+
 
     }
 }
